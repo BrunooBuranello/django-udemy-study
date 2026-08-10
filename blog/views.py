a196dcad70876657dfa2575352712c29f26c645f
BrunooBuranello <130617310+BrunooBuranello@users.noreply.github.com>
2026-08-10 01:18:13 -0300

    feat: add static files and template context

--- blog/views.py
+++ blog/views.py
@@ -4,17 +4,29 @@ from django.shortcuts import render
 # Create your views here.
 def blog(request):
     print("blog do app")
+
+    context ={
+        "text": "Olá blog",
+        }
+
     return render(
         request,
-        "blog/index.html"
+        "blog/index.html",
+        context
     )
 
 
 def exemplo(request):
     print("exemplo")
+
+    context ={
+        "text": "Olá exemplo",
+        }
+
     return render(
             request,
-            "blog/exemplo.html"
+            "blog/exemplo.html",
+            context,
         )
 
 
--- home/views.py
+++ home/views.py
@@ -4,8 +4,15 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     print("home")
+
+    context ={
+        "text": "Olá home",
+        "title": "Essa é uma página de exemplo - ",
+        }
+
     return render(
         request,
-        "home/index.html"
+        "home/index.html",
+        context,
     )
 
